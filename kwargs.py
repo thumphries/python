9a65af8d8105f0ee628c0b64a1530bc3f570aeb0
Timothy Humphries <tim@utf8.me>
2015-11-04 18:57:13 +1100

    zip fun

--- kwargs.py
+++ kwargs.py
@@ -11,9 +11,9 @@ def coolargs(**args):
             return
     print "I only accept cool arguments."
 
-coolargs(lame = "aww")
-coolargs(cool = "aww")
-coolargs(cool = 'cool')
+coolargs(lame = "aww")  # "I only accept cool arguments."
+coolargs(cool = "aww")  # "I only accept cool arguments."
+coolargs(cool = 'cool') # "Alright!"
 
 cooldict = { 'lame': 'aww', 'cool': 'cool' }
 # coolargs(cooldict) # not allowed - coolargs takes 0 arguments (1 given)
@@ -33,3 +33,25 @@ varargs(1, 2, 3, 4, 5, 6, 7, 8)
 coollist = [1, 2, 3]
 varargs(coollist)  # I got 1 args!
 varargs(*coollist) # I got 3 args!
+
+
+# fun with zip
+# zip works on iterables or argument sequences.
+# This one is regular usage on a list
+zipped = zip([1, 2, 3], [4, 5, 6], [7, 8, 9])
+print zipped # [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
+
+# Can zip up tuples easily
+print zip((1, 2, 3), (4, 5, 6))
+
+# This one is zipping the unpacked tuples together
+tups = zip(*zipped)
+print tups   # [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+
+# This one is real dumb - 'zipped' is a packed list of tups
+# and the second list doesn't exist
+# ... so we get this:
+print (zip(zipped)) # [((1, 4, 7),), ((2, 5, 8),), ((3, 6, 9),)]
+
+# ... even though zip usually truncates to the shortest argument:
+print (zip(zipped, [])) # []
